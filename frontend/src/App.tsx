d8d05fa800087a04812845930e6be8d4c74bfdde
Diogenes Oliveira <diogenesgomesads8@gmail.com>
2022-07-16 08:39:15 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
-
+import NotificationButton from "./components/NotificationButton"
 function App() {
-  return(
-    <h1>Ola mundo</h1>
+  return (
+    <>
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
